eceb2bbc57f723a0df933f2980f7c8acc107b045
999439 <999439@192.168.1.16>
2023-02-27 21:10:51 -0500

    a

---
+++ SmartChef/src/Main.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Main extends JPanel{
+	private JFrame frame;
+	private Container can;
+	
+	public Main() {
+		frame = new JFrame();
+		frame.setTitle("Login");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		can = frame.getContentPane();
+		
+		setPreferredSize(new Dimension(1500,800));
+		can.add(this);
+		
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
