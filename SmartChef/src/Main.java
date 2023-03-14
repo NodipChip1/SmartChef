bf9e137d768983a3e477db8f3780e118bf3880c7
c-eisenbach <126633820+c-eisenbach@users.noreply.github.com>
2023-03-13 20:49:22 -0400

    Update 3/13/23

--- SmartChef/src/Main.java
+++ SmartChef/src/Main.java
@@ -1,41 +1,61 @@
-import java.awt.*; 
-import java.awt.event.*;
-import java.io.InputStream;
-import javax.swing.*;
 
-public class MainScreen extends JPanel implements ActionListener{
-	/* Initializes all necessary variables
-	 */
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.Timer;
+
+public class Main extends JPanel implements ActionListener{
+	private int linkY = 400;
+	private String recipeName;
+	private GridLayout thelayout;
+    private GridBagConstraints gbc;
+	private ArrayList<JLabel> hyperLink = new ArrayList<JLabel>();
+	private boolean start, hypePageChange;
 	private JFrame frame;
+	private JPanel homePanel, panel2;
 	private Container can;
 	private JTextField password, recipeTitle;
-	private JTextArea ingredients;
+	private JTextArea ingredients, homeList;
+	private JScrollPane homeScroll;
 	private JButton loginEnterButton, back2Login, addRecipe, fav, enterRecipe, back2Home;
 	private Timer timer;
-	private boolean start;
 	private FontMetrics  fm;
 	private int page, fmWidth;
+	private BufferedWriter writer = null;
+	private String triedpass = "";
+	private ArrayList<String> recs = new ArrayList<String>();
 	public static final int HOME = 1, RECIPE = 3, MATCHES = 2, LOGIN = 0, FAV = 4, NEW=5, SUCCESS=6;
 	
+
+
+
+	
 	public Main() {
-		/* Creates JFrame and JPanel to be used for program
-		 * Declares and connects ActionListener to all JButtons, JTextFields, Hyperlinks, and TextAreas
-		 * 
-		 */
 		frame = new JFrame();
 		frame.setTitle("Login");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		can = frame.getContentPane();
+
+		thelayout = new GridLayout(3,2);
+	    this.setLayout(thelayout);
 		
 		setPreferredSize(new Dimension(1500,800));
+		this.setVisible(true);
 		can.add(this);
 		
-		frame.pack();
-	
-		this.setLayout(null);
+		panel2 = new Panel2(can);
+		//panel2.setVisible(false);
 		
-		password = new JTextField();
+		frame.pack();
+		
+		recipeName = new String(" ");
+		
+		password = new JTextField("123456");
+		password.setPreferredSize(new Dimension(100,200));
 		password.addActionListener(this);
+		
 		
 		loginEnterButton = new JButton("Enter");
 		loginEnterButton.addActionListener(this);
@@ -59,40 +79,61 @@ public class MainScreen extends JPanel implements ActionListener{
 		back2Home = new JButton("Back");
 		back2Home.addActionListener(this);
 		
+		homePanel = new JPanel();
+		homePanel.setLayout(new BorderLayout());
+		
+		homeList = new JTextArea();
+
+		homeList.setLayout(new BorderLayout());
+		homeList.setEditable(true);
+		
+		homeScroll = new JScrollPane(homeList);
+		homeScroll.setVerticalScrollBarPolicy (ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		
+		homePanel.setPreferredSize(new Dimension(800,500));
+		
+		homePanel.add(homeScroll, BorderLayout.CENTER);
+	
+		timer = new Timer(1000/60, this);
+		timer.addActionListener(this);
+		timer.start();
+		
+		hypePageChange = false;
+        
+		
+		
+        
+		
 		init();
 		
-		
 		frame.setVisible(true);
-		
+
 	}
 	private void init() {
-		page = LOGIN; //Declares int page which is used to record the page the user is on
-		start = true; //Boolean that allows the program to start at the login screen
+		page = LOGIN;
+		start = true;
 		
-	}
+												
+	        
+	    }
+		
 	public void paintComponent(Graphics g) {
-		/* Adds all necessary drawing elements to the program's interface
-		 * Includes: Text, Colors, Font
-		 * 
-		 * 
-		 */
 		super.paintComponent(g);
 		g.setColor(new Color(167,214,235));
 		g.fillRect(0,0,getWidth(), getHeight());
-		/* Downloads and implements Font JosefinSans to be used throughout the program
-		 */
-		String fName = "JosefinSans-VariableFont_wght.ttf"; //Input file of font
-	    InputStream is = MainScreen.class.getResourceAsStream(fName);
-	    Font font = null; //Initial font
+		
+		String fName = "JosefinSans-VariableFont_wght.ttf"; //Input file of font from Google
+	    InputStream is = Main.class.getResourceAsStream(fName);
+	
+	    Font font = null; //Tug 'O War font
 	   
-	    // copies of font that can be manipulated to have different sizing
+	    // copies of font using different sizes
 	    Font title = null;
 	    Font small = null;
 	    Font middle = null;
 	  
 	    try {
 			font = Font.createFont(Font.TRUETYPE_FONT, is);
-			//Setting the copies to different sizes
 			title = font.deriveFont(Font.BOLD, 75);
 			small = font.deriveFont(Font.BOLD, 30);
 			middle = font.deriveFont(Font.BOLD, 50);
@@ -100,7 +141,7 @@ public class MainScreen extends JPanel implements ActionListener{
 		} catch (Exception e) {}
 	    
 	    
-		if(page == LOGIN) { //Draws all necessary components for login screen
+		if(page == LOGIN) {
 			g.setColor(Color.BLACK);
 		    g.setFont(title);
 		    g.drawString("SmartChef",550,200);
@@ -109,19 +150,19 @@ public class MainScreen extends JPanel implements ActionListener{
 		    
 		}
 		
-		else if(page == HOME) {//Draws all necessary components for Home screen
+		else if(page == HOME) {
 			
 		}
-		else if(page == RECIPE) {//Draws all necessary components for Recipe screen
+		else if(page == RECIPE) {
 			
 		}
-		else if(page == MATCHES) {//Draws all necessary components for the matches screen
+		else if(page == MATCHES) {
 			
 		}
-		else if(page == FAV) {//Draws all necessary components for favorites screen
+		else if(page == FAV) {
 			
 		}
-		else if(page == NEW) {//Draws all necessary components for adding a new Recipe screen
+		else if(page == NEW) {
 			g.setColor(Color.BLACK);
 			g.setFont(title);
 			g.drawString("New Recipe", 550, 100);
@@ -130,7 +171,7 @@ public class MainScreen extends JPanel implements ActionListener{
 			g.drawString("Enter Ingredients here: Please seperate each ingredient by dropping to a new line", 150, 325);
 			
 		}
-		else if(page == SUCCESS) {//Draws all necessary components for login screen
+		else if(page == SUCCESS) {
 			g.setColor(Color.BLACK);
 			g.setFont(middle);
 			g.drawString("Your Recipe has been successfully addded!", 250, 400);
@@ -140,27 +181,46 @@ public class MainScreen extends JPanel implements ActionListener{
 	}
 	public void actionPerformed(ActionEvent e) {
 		Object source = e.getSource();
-		
-			setAllBounds(); //Declares sizes of JComponents
+		if(timer.isRunning()) {
+			setAllBounds(); //updates according to JFrame size
 			if(start) {
 				this.add(password);
 				this.add(loginEnterButton);
 				start = false;
+				repaint();
 			}
 			if(source.equals(back2Login)) {
 				init();
 				this.remove(back2Login);
 				this.remove(addRecipe);
 				this.remove(fav);
+				for(JLabel j: hyperLink) {
+					this.remove(j);
+				}
 			}
 			if(source.equals(loginEnterButton) || source.equals(password)) {
-				page = HOME;
-				this.add(back2Login);
-				this.add(addRecipe);
-				this.add(fav);
+				if(readPass("passwords.txt")) {
+					page = HOME;
 				
-				this.remove(loginEnterButton);
-				this.remove(password);
+					this.setVisible(false);
+					can.add(panel2);
+					panel2.repaint();
+					
+					
+					/*this.add(back2Login);
+					this.add(addRecipe);
+					this.add(homePanel);
+					this.add(fav);
+					*/
+					
+					
+					/*for(JLabel j: hyperLink) {
+						this.add(j);
+					}
+					
+					this.remove(loginEnterButton);
+					this.remove(password);*/
+				}
 			}
 			if(source.equals(addRecipe)) {
 				page = NEW;
@@ -172,6 +232,9 @@ public class MainScreen extends JPanel implements ActionListener{
 				this.remove(back2Login);
 				this.remove(addRecipe);
 				this.remove(fav);
+				for(JLabel j: hyperLink) {
+					this.remove(j);
+				}
 			}
 			if(source.equals(fav)) {
 				page = FAV;
@@ -181,6 +244,9 @@ public class MainScreen extends JPanel implements ActionListener{
 				this.remove(back2Login);
 				this.remove(addRecipe);
 				this.remove(fav);
+				for(JLabel j: hyperLink) {
+					this.remove(j);
+				}
 				
 			}
 			if(source.equals(back2Home)) {
@@ -188,6 +254,9 @@ public class MainScreen extends JPanel implements ActionListener{
 				this.add(back2Login);
 				this.add(addRecipe);
 				this.add(fav);
+				for(JLabel j: hyperLink) {
+					this.add(j);
+				}
 				
 				
 				this.remove(recipeTitle);
@@ -196,29 +265,146 @@ public class MainScreen extends JPanel implements ActionListener{
 				this.remove(back2Home);
 				
 			}
-			if(source.equals(enterRecipe)) {
-				page = SUCCESS;
+			
+			if(hypePageChange == true) {
+				this.add(back2Home);
+				this.remove(back2Login);
+				this.remove(addRecipe);
+				this.remove(fav);
+				for(JLabel j: hyperLink) {
+					this.remove(j);
+				}
+				hypePageChange = false;
+			}
+			
+			if(source.equals(enterRecipe) && recipeTitle.getText()!=null && ingredients.getText()!=null) {
+				recipeName = recipeTitle.getText();
+				hyperLink.add(new JLabel(recipeName));
+				for(JLabel j: hyperLink) {
+					j.setBounds(600,linkY,100,20);
+					linkY += 20;
+					j.addMouseListener(new MouseAdapter() {
+					public void mouseClicked(MouseEvent e) {
+				    	hypePageChange = true;
+				    }
+				    public void mouseEntered(MouseEvent e) {}
+				    public void mouseExited(MouseEvent e) {}
+					}
+					);
+				}
 				
+					 
+				    
+				page = SUCCESS;
 				this.remove(recipeTitle);
 				this.remove(ingredients);
 				this.remove(enterRecipe);
 				
 			}			
-			repaint();
+			
+			if(source.equals(enterRecipe)) {
+				RecRead();
+				addIngredients(ingredients.getText());
+				RecWrite();
+				while(recs.size() != 0) {
+					recs.remove(0);
+				}
+			}
+		//repaint();
 		}
+	}
 	private void setAllBounds() {
-		/*
-		 * sets all JButtons, JTextFields, JTextAreas etc to specific size and coordinates
-		 */
 		password.setBounds(550, 400, 400,50);
 		loginEnterButton.setBounds(1200,700,100,50);
-		back2Login.setBounds(0, 0, 100,50);
-		addRecipe.setBounds(1140,0,100,50);
-		fav.setBounds(1140,700,100,50);
+		back2Login.setBounds(10, 10, 100,50);
+		addRecipe.setBounds(1200,10,100,50);
+		fav.setBounds(1200,700,100,50);
 		recipeTitle.setBounds(550,200,400,50);
 		ingredients.setBounds(350,350,750,350);
 		enterRecipe.setBounds(1140,700,100,50);
 		back2Home.setBounds(10, 10, 100,50);
+		homePanel.setBounds(300,200,800,500);
+		
+	}
+	
+	public void addIngredients(String s) {
+		String[] parse = s.split("\n");
+		recs.add(recipeTitle.getText());
+		for(String s1: parse) {
+			
+			recs.add(s1);
+			
+		}
+		recs.add("n-der");
+
+	}
+
+	
+	public void RecRead(){
+
+		FileReader fr;
+		try {
+			fr = new FileReader("Recipie.txt");
+			Scanner in = new Scanner(fr); 
+			while(in.hasNext()){
+
+				String[] split = in.next().split(",");
+
+				for(String s: split) {
+
+					recs.add(s);
+
+				}
+
+				System.out.println(recs);
+
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void RecWrite()  {
+		FileWriter fw;
+		try {
+			fw = new FileWriter("Recipie.txt");
+			PrintWriter out = new PrintWriter(fw);
+
+			for(String s : recs) {
+				out.print(s);
+				//System.out.println(s);
+				if (s.equals("n-der")) {
+					out.println();
+				}
+				else {
+					out.print(",");
+				}
+			}
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public boolean readPass(String path){
+		boolean r = false;
+		try {
+			Scanner in = new Scanner(new FileReader(path));
+			while(in.hasNext()){
+				if(password.getText().equals(in.nextLine())) {
+					r = true;
+				}
+				
+			}
+		}
+		catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return r;
 	}
 
 
@@ -228,3 +414,5 @@ public class MainScreen extends JPanel implements ActionListener{
 	}
 
 }
+
+
